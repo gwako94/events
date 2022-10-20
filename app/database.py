2aac93af4fd8d56eb01a072285bc62c25ecc8804
gwako94 <galgallowako254@gmail.com>
2022-10-20 09:33:13 +0300

    test

--- app/database.py
+++ app/database.py
@@ -1,4 +1,5 @@
 import os
+import json
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
@@ -9,12 +10,15 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # load env variables
-google_credentials = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+google_credentials = os.getenv("GOOGLE_CREDENTIALS")
 databaseURL = os.getenv("DATABASE_URL")
+
+with open("google_credentials.json", "w") as file:
+    json.dump(google_credentials, file)
 
 
 # fetch google certificate
-cred = credentials.Certificate(google_credentials)
+cred = credentials.Certificate('google_credentials.json')
 
 
 # Initialize Firebase
